bb289a6c901d3f02d9787b482ac70f1ece5f0707
hannita1 <hannah.bussmann@studserv.uni-leipzig.de>
2024-05-03 08:38:28 +0200

    test: testing environments

--- docs/source/tutorials/env_test.py
+++ docs/source/tutorials/env_test.py
@@ -1,6 +1,7 @@
 from myosuite.utils import gym
 env = gym.make('myoHandPoseFixed-v0')
 env.reset()
+
 for _ in range(1000):
   env.mj_render()
   env.step(env.action_space.sample()) # take a random action
