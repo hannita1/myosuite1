f6860691271c6b612e081d650c84fe8ab398e9f0
hannita1 <hannah.bussmann@studserv.uni-leipzig.de>
2024-05-03 10:07:52 +0200

    loading policy works

--- docs/source/tutorials/train2.py
+++ docs/source/tutorials/train2.py
@@ -17,23 +17,23 @@ env.reset()
 model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logs_dir)
 
 TIMESTEPS = 10000
-for i in range(1,30):
-    model.learn(total_timesteps=TIMESTEPS)#, reset_num_timesteps=False, tb_log_name='PPO')
+#for i in range(1,30):
+#    model.learn(total_timesteps=TIMESTEPS)#, reset_num_timesteps=False, tb_log_name='PPO')
     #save model every 10000 steps
-    model.save(f"{models_dir}/{TIMESTEPS*i}")
+#    model.save(f"{models_dir}/{TIMESTEPS*i}")
 
 model_path = f"{models_dir}/170000.zip"
 
 pi = PPO.load(model_path, env=env)
 
-for ep in range(1,300):
+for ep in range(1,3000):
     obs = env.reset()
     done = False
-    while not done:
-        env.mj_render()
-        obs = env.get_obs()
-        #action = model.get_action(obs)[0]
-        action, _ = pi.predict(obs)
-        next_o, r, done, *_, ifo = env.step(action)
+    #while not done:
+    env.mj_render()
+    obs = env.get_obs()
+    #action = model.get_action(obs)[0]
+    action, _ = pi.predict(obs)
+    next_o, r, done, *_, ifo = env.step(action)
 
 env.close()
