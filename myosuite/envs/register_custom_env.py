d9cba46d3f569d122190287b24482513d980cbdf
hannita1 <hannah.bussmann@studserv.uni-leipzig.de>
2024-07-08 09:40:42 +0200

    added xml files for tabletop environment, simple smartphone with 3 touch areas on table

--- myosuite/envs/register_custom_env.py
+++ myosuite/envs/register_custom_env.py
@@ -1,4 +1,5 @@
 from myosuite.utils import gym; register=gym.register
+from myosuite.envs.myo.myobase import register_env_with_variants
 from my_hand_env import MyHandEnv
 import os
 curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -25,6 +26,16 @@ base_env_name = "MyHandEnv-v0"
 base_env_variants = {
     'max_episode_steps': 200,  # Episode length
 }
+
+
+#register_env_with_variants(id='myoHandObjHoldRandom-v0', # revisit
+#        entry_point='myosuite.envs.myo.myobase.obj_hold_v0:ObjHoldRandomEnvV0',
+#        max_episode_steps=75,
+#        kwargs={
+#            'model_path': curr_dir+'/../assets/hand/myohand_hold.xml',
+#            'normalize_act': True
+#        }
+#    )
 
 variant_env_name = register_env_variant(env_id=base_env_name, variants=base_env_variants)
 
