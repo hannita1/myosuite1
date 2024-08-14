51d1befb10c5d7a3fc99c2da0a330e910e963649
hannita1 <hannah.bussmann@studserv.uni-leipzig.de>
2024-08-14 14:42:54 +0200

    deleted unused files

--- docs/source/tutorials/test3.py
+++
@@ -1,8 +0,0 @@
-import myosuite
-import gymnasium as gym
-env = gym.make('myoElbowPose1D6MRandom-v0')
-env.reset()
-for _ in range(1000):
-  env.mj_render()
-  env.step(env.action_space.sample()) # take a random action
-env.close()
